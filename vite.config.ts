aed6b6575b0c3d56d6a30b66b9266afcd6ce6c29
tahirdibirov <tahirdibirovsecond@gmail.com>
2024-08-14 17:57:40 +0400

    INIT

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import fs from 'fs'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   server: {
-    https: {
-      key: fs.readFileSync('cert.key'),
-      cert: fs.readFileSync('cert.crt'),
-    },
-    host: true,
-    port: 5173,
+    host: true
   },
+  plugins: [react()],
 })
